512d7deda8bcc7910d3e130a66466acf0984717f
Mashannn <105122498+Mashannn@users.noreply.github.com>
2022-08-06 19:35:21 +0700

    Update main.js

--- main.js
+++ main.js
@@ -167,8 +167,8 @@ global.reloadHandler = function (restatConn) {
     conn.ev.off('creds.update', conn.credsUpdate)
   }
 
-  conn.welcome = 'HALO @user 👋\n SELAMAT DATANG DI  @subject \n❋𝗝𝗔𝗦𝗔 𝗥𝗨𝗡❋\n-𝚃𝚒𝚗𝚐𝚐𝚊𝚕 𝚂𝚌𝚊𝚗\n-𝟸𝟺𝙹𝚊𝚖 𝚋𝚘𝚝 𝚊𝚔𝚝𝚒𝚏\n𝙿𝚊𝚢𝚖𝚎𝚗𝚝 : 𝙳𝙰𝙽𝙰\n𝙼𝚒𝚗𝚊𝚝 ? wa.me/6287898307350' 
-  conn.bye = 'YAHHH @user TELAH KELUAR DARI @subject'
+  conn.welcome = 'HALO @user 👋\n' 
+  conn.bye = 'YAHHH @user Kamu Keluar Grub 😔'
   conn.spromote = '@user sekarang admin!'
   conn.sdemote = '@user sekarang bukan admin!'
   conn.handler = handler.handler.bind(conn)
